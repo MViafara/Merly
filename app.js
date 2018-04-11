b01de7db08a95951fa10cdab375a0b6e202a2549
Mviafara <merly1012@hotmail.com>
2018-04-11 17:57:44 -0500

    cambio3 index y app

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 firebase.initializeApp({
 apiKey: "AIzaSyDNcVFDDpOa2wm6KrT3pwhG8Vk2jZjImMU",
 authDomain: "openshisft.firebaseapp.com",
-storageBucket: "openshisft.appspot.com",
+projectId: "openshisft",
+});
+
+var db = firebase.firestore();
+
+
+db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
 });
